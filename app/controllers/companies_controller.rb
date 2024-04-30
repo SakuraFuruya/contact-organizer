4831f3e3c436efd922242276843662dc0b5e1f20
SakuraFuruya <cherryblossom.sakura.f@gmail.com>
2024-04-30 09:59:47 -0600

    implemented controllers

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,25 @@
 class CompaniesController < ApplicationController
+
+  def index
+    @companies = Company.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
