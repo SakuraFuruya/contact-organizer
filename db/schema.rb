06c231b0bcdbcab7b81053f8262cbd45cfad25c4
SakuraFuruya <cherryblossom.sakura.f@gmail.com>
2024-04-30 17:41:31 -0600

    implemented devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_30_005025) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_30_232357) do
   create_table "companies", force: :cascade do |t|
     t.string "company_name"
     t.string "industry"
@@ -27,6 +27,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_30_005025) do
     t.datetime "updated_at", null: false
     t.integer "company_id"
     t.index ["company_id"], name: "index_employees_on_company_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "employees", "companies"
