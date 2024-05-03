e0e43b4a22a1ccb49d7340b7b8c9fe2a5c65b5ca
SakuraFuruya <cherryblossom.sakura.f@gmail.com>
2024-05-03 15:36:04 -0600

    implemented js

--- app/javascript/controllers/edit_controller.js
+++ app/javascript/controllers/edit_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit"
 export default class extends Controller {
-  connect() {
+
+  static targets = ["content", "form"]
+
+  displayForm(){
+    console.log("Hello")
+    this.formTarget.classList.remove("d-none")
+    // this.contentTarget.classList.add("d-none")
   }
+
 }
